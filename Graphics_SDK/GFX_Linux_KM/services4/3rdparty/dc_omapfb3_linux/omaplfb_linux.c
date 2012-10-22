4bb7ed1572fc61772e3aeecce0cf338dcbada814
Robert Nelson <robertcnelson@gmail.com>
2012-10-22 15:40:46 -0500

    sgx: with 2.6.38 WQ_FREEZEABLE -> WQ_FREEZABLE http://git.kernel.org/?p=linux/kernel/git/torvalds/linux.git;a=commit;h=58a69cb47ec6991bf006a3e5d202e8571b0327a4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- Graphics_SDK/GFX_Linux_KM/services4/3rdparty/dc_omapfb3_linux/omaplfb_linux.c
+++ Graphics_SDK/GFX_Linux_KM/services4/3rdparty/dc_omapfb3_linux/omaplfb_linux.c
@@ -281,7 +281,12 @@ OMAPLFB_ERROR OMAPLFBCreateSwapQueue(OMAPLFB_SWAPCHAIN *psSwapChain)
 	 * conditions, preventing the driver from holding on to
 	 * resources longer than it needs to.
 	 */
+#if (LINUX_VERSION_CODE == KERNEL_VERSION(2,6,37))
 	psSwapChain->psWorkQueue = alloc_ordered_workqueue(DEVNAME, WQ_FREEZEABLE | WQ_MEM_RECLAIM);
+#else
+	psSwapChain->psWorkQueue = alloc_ordered_workqueue(DEVNAME, WQ_FREEZABLE | WQ_MEM_RECLAIM);
+#endif
+
 #else
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,36))
 	psSwapChain->psWorkQueue = create_freezable_workqueue(DEVNAME);
